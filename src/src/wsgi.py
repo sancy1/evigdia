2570b96235e5cc7aa3137648cb5bad13dbddf7c1
Alexander Cyril <sanchez.alexander.cyril@com>
2025-05-09 03:23:43 +0100

    Added update

--- src/src/wsgi.py
+++ src/src/wsgi.py
@@ -1,16 +1,24 @@
-"""
-WSGI config for src project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.src.settings')
+# Check for a specific environment variable that indicates production (Render)
+if 'RENDER' in os.environ:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.src.settings')
+else:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.settings')
 
 application = get_wsgi_application()
+
+
+
+
+# import os
+
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.src.settings')
+
+# application = get_wsgi_application()
